1b970d3c0731f7e45bee265ced6c36ab32b8f6b7
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-04-20 11:04:44 +0100

    Update required glue-core to 0.10.3 and update version number to 0.10.3

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ from setuptools import setup
 with open('README.rst') as infile:
     LONG_DESCRIPTION = infile.read()
 
-install_requires = ['glue-core>=0.10.2',
+install_requires = ['glue-core>=0.10.3',
                     'glue-vispy-viewers>=0.7.2']
 
 setup(name='glueviz',
-      version='0.10.2',
+      version='0.10.3',
       description='Multidimensional data visualization across files',
       long_description=LONG_DESCRIPTION,
       author='Glue developers',
