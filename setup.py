ca9e480391192c84c77ac5d9e2cda5d784404a63
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-10-30 17:35:29 -0400

    Update version to v0.12.1

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ from setuptools import setup
 with open('README.rst') as infile:
     LONG_DESCRIPTION = infile.read()
 
-install_requires = ['glue-core>=0.12.0',
+install_requires = ['glue-core>=0.12.1',
                     'glue-vispy-viewers>=0.9']
 
 setup(name='glueviz',
-      version='0.12.0',
+      version='0.12.1',
       description='Multidimensional data visualization across files',
       long_description=LONG_DESCRIPTION,
       author='Glue developers',
